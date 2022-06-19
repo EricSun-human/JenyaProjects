f4afb556385aad1a49f6f7ecaeeac30135aecced
EricSun-human <85909186+EricSun-human@users.noreply.github.com>
2022-06-19 10:52:45 -0700

    Add files via upload

--- arrayEvaluation.py
+++ arrayEvaluation.py
@@ -1,14 +1,19 @@
 import numpy as np
 
 arrayOne = np.array([[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]])
-arrayTwo = np.array([[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]])
-arrayThree = np.array([[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]])
+arrayTwo = np.array([[5,6,7,8,9],[5,6,7,8,9],[5,6,7,8,9],[5,6,7,8,9],[5,6,7,8,9],[5,6,7,8,9]])
+#arrayThree = np.array([[1,1,1,1,1][2,2,2,2,2],[3,3,3,3,3],[4,4,4,4,4],[5,5,5,5,5]])
+
+arrayOne = np.arange(0,5*6,1).reshape((5,6))
+arrayTwo = np.arange(0,5*6,1).reshape((5,6))+30
+arrayThree = np.arange(0,5*6,1).reshape((5,6))+60
+
 print(arrayOne.shape)
 print(arrayOne.ndim)
-#combinedArray = np.concatenate(arrayOne,arrayTwo)
-combinedArray = np.append(arrayOne,arrayTwo)
-combinedArray = np.append(combinedArray,arrayThree)
+combinedArray = np.stack((arrayOne,arrayTwo,arrayThree),axis=2)
+#combinedArray = np.append(arrayOne,arrayTwo)
+#combinedArray = np.append(combinedArray,arrayThree)
 print(combinedArray)
-combinedArray = np.reshape(combinedArray,(3,6,3))
-print(combinedArray)
-print(combinedArray.ndim)
+#combinedArray = np.reshape(combinedArray,(3,6,3))
+#print(combinedArray)
+print(combinedArray.shape)
