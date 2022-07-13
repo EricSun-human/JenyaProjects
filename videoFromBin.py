bea4f24355447858aa2fb81978aa816fcba5ab2c
EricSun-human <85909186+EricSun-human@users.noreply.github.com>
2022-07-12 20:36:38 -0700

    Update videoFromBin.py

--- videoFromBin.py
+++ videoFromBin.py
@@ -2,12 +2,15 @@ import numpy as np
 
 def get_data_from_annotation_array(height, width, annotation_buffer):
     '''returns array with dims: height, width, numFrames'''
+    print("annotation_buffer type: "+str(type(annotation_buffer)))
+    #print("annotation_buffer len: "+str(len(annotation_buffer)))
+    print("annotation_buffer type"+str(type(annotation_buffer)))
     annotation = np.frombuffer(annotation_buffer, dtype=np.uint8) # Get error on this line-->TypeError: memoryview: a bytes-like object is required, not '_io.BufferedReader'
     annotation_np = annotation.reshape(width, height, -1, order='F').swapaxes(0, 1)
     return annotation_np.astype('float32')
 
 output_file = open("C:\\Users\\erics\\Downloads\\jenya_4b3962e8-faba-4535-9c35-b3df955f2c9e_5_x264.bin", 'rb')
-#print(output_file.shape)
+print("output_file data:"+str(output_file))
 #for line in output_file:
     #print("\n"+str(line))
 
