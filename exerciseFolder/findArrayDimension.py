f0da910f2a7966f33214d101314279b11eafbcb2
EricSun-human <85909186+EricSun-human@users.noreply.github.com>
2022-07-23 19:10:30 -0700

    Create findArrayDimension.py

---
+++ exerciseFolder/findArrayDimension.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+oneDArray = np.array([1,2,3])
+twoDArray = np.array([[1,2,3],[1,2,3]])
+threeDArray = np.array([[[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]]])
+print(oneDArray.ndim)
+print(twoDArray.ndim)
+print(threeDArray.ndim)
