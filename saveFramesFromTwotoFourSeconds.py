3298e34f73aa79499c65273f1e445ddeb5dacce8
EricSun-human <85909186+EricSun-human@users.noreply.github.com>
2022-07-23 18:59:32 -0700

    Delete saveFramesFromTwotoFourSeconds.py

--- saveFramesFromTwotoFourSeconds.py
+++
@@ -1,7 +0,0 @@
-import ffmpeg
-#import cv2
-#import subprocess as sp
-import os
-os.system("ffmpeg -i rickroll.mp4 -ss 00:00:02 -t 00:00:02 -async 1 -strict -2 cutRickRoll.mp4")
-os.system("cd cutRickroll")
-os.system("ffmpeg -i cutRickRoll.mp4 -r 24/1 out%03d.jpg")
