ae1c0b0efa25d21a21c06cfe4205b3d5dfe8d819
EricSun-human <85909186+EricSun-human@users.noreply.github.com>
2022-07-23 19:10:37 -0700

    Delete findArrayDimension.py

--- findArrayDimension.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-oneDArray = np.array([1,2,3])
-twoDArray = np.array([[1,2,3],[1,2,3]])
-threeDArray = np.array([[[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]]])
-print(oneDArray.ndim)
-print(twoDArray.ndim)
-print(threeDArray.ndim)
