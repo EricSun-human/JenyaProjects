3155c2c553511c09f9176fec49f3d4080cd3b5b3
EricSun-human <85909186+EricSun-human@users.noreply.github.com>
2022-07-23 19:06:50 -0700

    Delete changeVideoFormats.py

--- changeVideoFormats.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system("cd OneDrive")
-os.system("cd Desktop")
-os.system("ffmpeg -i RickRoll.mp4 -c:v rawvideo convertedToRAW.raw"
-#Converts w/out raising error, but yuv files are not openable w/ YUV reader
-os.system("ffmpeg -i RickRoll.mp4 -c:v rawvideo convertedToRAW.yuv"))
-os.system("ffmpeg -i RickRoll.mp4 convertedToRAW.yuv")
