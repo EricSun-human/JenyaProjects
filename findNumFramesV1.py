f18379c98669ab257ca735adb69fe1acdd49688d
EricSun-human <85909186+EricSun-human@users.noreply.github.com>
2022-07-23 19:08:37 -0700

    Delete findNumFramesV1.py

--- findNumFramesV1.py
+++
@@ -1,5 +0,0 @@
-import cv2
-danceVideo = cv2.VideoCapture("C:\\Users\\erics\\OneDrive\\Desktop\\RickRoll.mp4")
-#counts the number of frames using cv2.CAP_PROP_FRAME_COUNT and then prints the number
-lenVideo= danceVideo.get(cv2.CAP_PROP_FRAME_COUNT)
-print("The number of frames is: "+str(lenVideo))
